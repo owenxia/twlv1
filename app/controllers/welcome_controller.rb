f978e890fe1b7cc8e989d757b9a442d581f10d06
Owen Xia <owenyx@gmail.com>
2015-05-10 13:02:49 +0800

    View - navigation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+  	
+  end
+  def inspirations
+  	
+  end
+  def how_it_works
+  	
+  end
+  def my_wish_list
+  	
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
 
+  get '/travel-inspirations', to: 'welcome#inspirations'
+  get '/how-it-works', to: 'welcome#how_it_works'
+  get '/my-wish-list', to: 'welcome#my_wish_list'
+
   resources :profiles do
     member do
 	    get :follow
