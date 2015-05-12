ce18c8548f51dd209b1f4552efd1bd407cff10b6
Owen Xia <owenyx@gmail.com>
2015-05-12 10:40:01 +0800

    Comment styling

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :experience
+  belongs_to :user
+
+  def set_user(id)
+  	User.find(id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512005205) do
+ActiveRecord::Schema.define(version: 20150512021516) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "commentable_id"
