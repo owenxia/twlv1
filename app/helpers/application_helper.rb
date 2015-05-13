0f69f0a7d72cc24d8a2993ef11f9197c644e1044
Owen Xia <owenyx@gmail.com>
2015-05-13 16:11:28 +0800

    Bug fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 	    profile.avatar_url
 	  else
 	    default_url = "#{root_url}images/felix-the-cat.gif"
-	    gravatar_id = Digest::MD5.hexdigest(profile.user.email.downcase)
+	    gravatar_id = Digest::MD5.hexdigest(profile.user.email.downcase) 
 	    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
 	  end
 	end
