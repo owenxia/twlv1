8f8afba77ed0270d3ba283f6ffa9a5a87ce98258
Owen Xia <owenyx@gmail.com>
2015-05-16 08:38:47 +0800

    Removed sending email from production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,19 +78,19 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_url_options = { :host => 'http://travelwishlists.com' }
-config.action_mailer.delivery_method = :smtp 
-config.action_mailer.smtp_settings = {
-address: "smtp.gmail.com",
-port: 587,
-domain: ENV["GMAIL_DOMAIN"],
-authentication: "plain",
-enable_starttls_auto: true,
-user_name: ENV["GMAIL_USERNAME"],
-password: ENV["GMAIL_PASSWORD"]
-}
+# config.action_mailer.perform_deliveries = true
+# config.action_mailer.raise_delivery_errors = true
+# config.action_mailer.default_url_options = { :host => 'http://travelwishlists.com' }
+# config.action_mailer.delivery_method = :smtp 
+# config.action_mailer.smtp_settings = {
+# address: "smtp.gmail.com",
+# port: 587,
+# domain: ENV["GMAIL_DOMAIN"],
+# authentication: "plain",
+# enable_starttls_auto: true,
+# user_name: ENV["GMAIL_USERNAME"],
+# password: ENV["GMAIL_PASSWORD"]
+# }
 
 
 end
