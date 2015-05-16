e65c946e7dd81180183ad0eb0c093bcb4b232a41
Owen Xia <owenyx@gmail.com>
2015-05-16 09:09:54 +0800

    Everything works locally

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ address: "smtp.gmail.com",
 port: 587,
 domain: ENV["GMAIL_DOMAIN"],
 authentication: "plain",
-enable_starttls_auto: true,
+enable_starttls_auto: false,
 user_name: ENV["GMAIL_USERNAME"],
 password: ENV["GMAIL_PASSWORD"]
 }
